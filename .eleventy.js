6901fc9ec134c34d04a98fc68350303b8e8211be
rosematcha <120700018+rosematcha@users.noreply.github.com>
2025-01-30 15:48:24 -0600

    Initial, funsies-focused commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css"); // add css folder
+    eleventyConfig.addWatchTarget("./src/css/"); // watch css for changes
+    eleventyConfig.addPassthroughCopy("./src/assets"); // add assets folder
+    eleventyConfig.addWatchTarget("./src/assets/"); // watch assets for changes
+    return {
+      dir: {
+        input: "src",
+        output: "public",
+      },
+    };
+  };
---
+++ src/dues.md
@@ -0,0 +1,7 @@
+---
+title: Donations
+layout: base.njk
+tags: nav
+---
+
+To renew your annual membership with the Tennison Society, please blow up.
